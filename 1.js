591b890f103100f7707ccbf72975563e17779ef7
x12xX <91122402+x12xX@users.noreply.github.com>
2021-09-21 13:15:55 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,14 @@
+function understands_video() {
+    return !!document.createElement('video').canPlayType; // boolean
+    }
+    
+    if ( !understands_video() ) {
+    // Must be older browser or IE.
+    // Maybe do something like hide custom
+    // HTML5 controls. Or whatever...
+<<<<<<< HEAD
+    videoControls.style.display = 'none'; ggg
+=======
+    videoControls.style.display = 'none'; hkj
+>>>>>>> B2
+    }
